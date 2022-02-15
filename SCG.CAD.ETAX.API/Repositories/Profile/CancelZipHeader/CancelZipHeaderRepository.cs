c5375358e23bacbed96886c7c14d4424f1dc3bf4
PICHET <pichet_chujit@hotmail.co.th>
2022-02-15 10:43:46 +0700

    adding API CancelZipHeader

--- SCG.CAD.ETAX.API/Repositories/Profile/CancelZipHeader/CancelZipHeaderRepository.cs
+++ SCG.CAD.ETAX.API/Repositories/Profile/CancelZipHeader/CancelZipHeaderRepository.cs
@@ -4,29 +4,99 @@
     {
         CancelZipHeaderService service = new CancelZipHeaderService();
 
-        public Task<Response> DELETE(MODEL.CancelZipHeader param)
+        public async Task<Response> GET_DETAIL(int id)
         {
-            throw new NotImplementedException();
+            Response resp = new Response();
+
+            try
+            {
+                var result = service.GET_DETAIL(id);
+
+                resp = result;
+            }
+            catch (Exception ex)
+            {
+                resp.STATUS = false;
+                resp.ERROR_MESSAGE = ex.InnerException.Message.ToString();
+            }
+
+            return await Task.FromResult(resp);
         }
 
-        public Task<Response> GET_DETAIL(int id)
+        public async Task<Response> GET_LIST()
         {
-            throw new NotImplementedException();
+            Response resp = new Response();
+
+            try
+            {
+                var result = service.GET_LIST();
+
+                resp = result;
+            }
+            catch (Exception ex)
+            {
+                resp.STATUS = false;
+                resp.ERROR_MESSAGE = ex.InnerException.Message.ToString();
+            }
+
+            return await Task.FromResult(resp);
         }
 
-        public Task<Response> GET_LIST()
+        public async Task<Response> INSERT(MODEL.CancelZipHeader param)
         {
-            throw new NotImplementedException();
+            Response resp = new Response();
+
+            try
+            {
+                var result = service.INSERT(param);
+
+                resp = result;
+            }
+            catch (Exception ex)
+            {
+                resp.STATUS = false;
+                resp.ERROR_MESSAGE = ex.InnerException.Message.ToString();
+            }
+
+            return await Task.FromResult(resp);
         }
 
-        public Task<Response> INSERT(MODEL.CancelZipHeader param)
+        public async Task<Response> UPDATE(MODEL.CancelZipHeader param)
         {
-            throw new NotImplementedException();
+            Response resp = new Response();
+
+            try
+            {
+                var result = service.UPDATE(param);
+
+                resp = result;
+            }
+            catch (Exception ex)
+            {
+                resp.STATUS = false;
+                resp.ERROR_MESSAGE = ex.InnerException.Message.ToString();
+            }
+
+            return await Task.FromResult(resp);
         }
 
-        public Task<Response> UPDATE(MODEL.CancelZipHeader param)
+        public async Task<Response> DELETE(MODEL.CancelZipHeader param)
         {
-            throw new NotImplementedException();
+            Response resp = new Response();
+
+            try
+            {
+                var result = service.DELETE(param);
+
+                resp = result;
+            }
+            catch (Exception ex)
+            {
+                resp.STATUS = false;
+                resp.ERROR_MESSAGE = ex.InnerException.Message.ToString();
+            }
+
+            return await Task.FromResult(resp);
         }
     }
 }
--- SCG.CAD.ETAX.API/Services/Profile/CancelZipHeader/CancelZipHeaderService.cs
+++ SCG.CAD.ETAX.API/Services/Profile/CancelZipHeader/CancelZipHeaderService.cs
@@ -4,82 +4,156 @@
     {
 
         readonly DatabaseContext _dbContext = new();
-        public List<CancelZipHeader> GET_LIST()
+        public Response GET_LIST()
         {
-            List<CancelZipHeader> resp = new List<CancelZipHeader>();
+            Response resp = new Response();
             try
             {
+                var getList = _dbContext.cancelZipHeader.ToList();
+
+                if (getList.Count > 0)
+                {
+                    resp.STATUS = true;
+                    resp.MESSAGE = "Get list count '" + getList.Count + "' records. ";
+                    resp.OUTPUT_DATA = getList;
+                }
+                else
+                {
+                    resp.STATUS = false;
+                    resp.MESSAGE = "Data not found";
+                }
 
             }
-            catch
+            catch (Exception ex)
             {
-                throw;
+                resp.STATUS = false;
+                resp.MESSAGE = "Get data fail.";
+                resp.INNER_EXCEPTION = ex.InnerException.ToString();
             }
             return resp;
         }
 
-        public List<CancelZipHeader> GET_DETAIL(int id)
+        public Response GET_DETAIL(int id)
         {
-            List<CancelZipHeader> resp = new List<CancelZipHeader>();
+            Response resp = new Response();
+
             try
             {
+                var getList = _dbContext.cancelZipHeader.Where(x => x.CancelZipHeaderNo == id).ToList();
+
+                if (getList.Count > 0)
+                {
+                    resp.STATUS = true;
+                    resp.MESSAGE = "Get data from ID '" + id + "' success. ";
+                    resp.OUTPUT_DATA = getList;
+                }
+                else
+                {
+                    resp.STATUS = false;
+                    resp.MESSAGE = "Data not found";
+                }
 
             }
-            catch
+            catch (Exception ex)
             {
-                throw;
+                resp.STATUS = false;
+                resp.MESSAGE = "Get data fail.";
+                resp.INNER_EXCEPTION = ex.InnerException.ToString();
             }
             return resp;
         }
 
-        public List<CancelZipHeader> INSERT(CancelZipHeader param)
+        public Response INSERT(CancelZipHeader param)
         {
-            List<CancelZipHeader> resp = new List<CancelZipHeader>();
+            Response resp = new Response();
             try
             {
                 using (_dbContext)
                 {
+                    _dbContext.cancelZipHeader.Add(param);
+                    _dbContext.SaveChanges();
 
-
+                    resp.STATUS = true;
+                    resp.MESSAGE = "Insert success.";
                 }
             }
-            catch
+            catch (Exception ex)
             {
-                throw;
+                resp.STATUS = false;
+                resp.MESSAGE = "Insert faild.";
+                resp.INNER_EXCEPTION = ex.InnerException.ToString();
             }
             return resp;
         }
 
-        public List<CancelZipHeader> UPDATE(CancelZipHeader param)
+        public Response UPDATE(CancelZipHeader param)
         {
-            List<CancelZipHeader> resp = new List<CancelZipHeader>();
+            Response resp = new Response();
             try
             {
                 using (_dbContext)
                 {
+                    var update = _dbContext.cancelZipHeader.Where(x => x.CancelZipHeaderNo == param.CancelZipHeaderNo).FirstOrDefault();
 
+                    if (update != null)
+                    {
+                        update.StatusCancel = param.StatusCancel;
+                        update.WorkFlowCode = param.WorkFlowCode;
+                        update.CancelReason = param.CancelReason;
+                        update.UpdateBy = param.UpdateBy;
+                        update.UpdateDate = param.UpdateDate;
+                        update.Isactive = param.Isactive;
+
+                        _dbContext.SaveChanges();
+
+                        resp.STATUS = true;
+                        resp.MESSAGE = "Updated Success.";
+                    }
+                    else
+                    {
+                        resp.STATUS = false;
+                        resp.MESSAGE = "Can't update because data not found.";
+                    }
                 }
             }
-            catch
+            catch (Exception ex)
             {
-                throw;
+                resp.STATUS = false;
+                resp.MESSAGE = "Update faild.";
+                resp.INNER_EXCEPTION = ex.InnerException.ToString();
             }
             return resp;
         }
 
-        public List<CancelZipHeader> DELETE(CancelZipHeader param)
+        public Response DELETE(CancelZipHeader param)
         {
-            List<CancelZipHeader> resp = new List<CancelZipHeader>();
+            Response resp = new Response();
             try
             {
                 using (_dbContext)
                 {
+                    var delete = _dbContext.cancelZipHeader.Find(param.CancelZipHeaderNo);
 
+                    if (delete != null)
+                    {
+                        _dbContext.cancelZipHeader.Remove(delete);
+                        _dbContext.SaveChanges();
+
+                        resp.STATUS = true;
+                        resp.MESSAGE = "Delete success.";
+                    }
+                    else
+                    {
+                        resp.STATUS = false;
+                        resp.MESSAGE = "Can't delete because data not found.";
+                    }
                 }
             }
-            catch
+            catch (Exception ex)
             {
-                throw;
+                resp.STATUS = false;
+                resp.MESSAGE = "Update faild.";
+                resp.INNER_EXCEPTION = ex.InnerException.ToString();
             }
             return resp;
         }
