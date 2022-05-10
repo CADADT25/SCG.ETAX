dac7d9ec8b7387ee65446cb3a127039f3689ca26
CADVPNAP14 CAD <CADVPNAP14@cementhai.com>
2022-05-10 16:03:05 +0700

    initial OutputSearchPrinting

---
+++ SCG.CAD.ETAX.WEB/Controllers/Etax/OutputSearchPrintingController.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SCG.CAD.ETAX.WEB.Controllers.Etax
+{
+    public class OutputSearchPrintingController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult _Content()
+        {
+            return View();
+        }
+
+        public IActionResult _Modal()
+        {
+            return View();
+        }
+
+        public IActionResult _Create()
+        {
+            return View();
+        }
+
+        public IActionResult _Update()
+        {
+            return View();
+        }
+
+        public async Task<JsonResult> Detail(int id)
+        {
+            List<OutputSearchPrinting> tran = new List<OutputSearchPrinting>();
+
+            var task = await Task.Run(() => ApiHelper.GetURI("api/OutputSearchPrinting/GetDetail?id= " + id + " "));
+
+            Response resp = new Response();
+
+            var result = "";
+
+            if (task.STATUS)
+            {
+
+                tran = JsonConvert.DeserializeObject<List<OutputSearchPrinting>>(task.OUTPUT_DATA.ToString());
+
+                result = JsonConvert.SerializeObject(tran[0]);
+
+            }
+            else
+            {
+                ViewBag.Error = task.MESSAGE;
+            }
+            return Json(result);
+        }
+
+        public async Task<JsonResult> List()
+        {
+            Response resp = new Response();
+
+            List<OutputSearchPrinting> tran = new List<OutputSearchPrinting>();
+
+            try
+            {
+                var task = await Task.Run(() => ApiHelper.GetURI("api/OutputSearchPrinting/GetListAll"));
+
+                if (task.STATUS)
+                {
+                    tran = JsonConvert.DeserializeObject<List<OutputSearchPrinting>>(task.OUTPUT_DATA.ToString());
+                }
+                else
+                {
+                    ViewBag.Error = task.MESSAGE;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+
+
+            return Json(new { data = tran });
+        }
+
+        public async Task<JsonResult> Insert(string jsonString)
+        {
+            Response res = new Response();
+
+            var httpContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            var task = await Task.Run(() => ApiHelper.PostURI("api/OutputSearchPrinting/Insert", httpContent));
+
+            return Json(task);
+        }
+
+        public async Task<JsonResult> Update(string jsonString)
+        {
+            Response res = new Response();
+
+            var httpContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            var task = await Task.Run(() => ApiHelper.PostURI("api/OutputSearchPrinting/Update", httpContent));
+
+            return Json(task);
+        }
+
+        public async Task<JsonResult> Delete(string jsonString)
+        {
+            Response res = new Response();
+
+            var httpContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            var task = await Task.Run(() => ApiHelper.PostURI("api/OutputSearchPrinting/Delete", httpContent));
+
+            return Json(task);
+        }
+
+        public async Task<ActionResult> ExportToCsv()
+        {
+            Response resp = new Response();
+
+            List<OutputSearchPrinting> tran = new List<OutputSearchPrinting>();
+
+            var strBuilder = new StringBuilder();
+
+            try
+            {
+                var task = await Task.Run(() => ApiHelper.GetURI("api/OutputSearchPrinting/GetListAll"));
+
+                if (task.STATUS)
+                {
+                    tran = JsonConvert.DeserializeObject<List<OutputSearchPrinting>>(task.OUTPUT_DATA.ToString());
+
+                    if (tran.Count() > 0)
+                    {
+                        strBuilder.AppendLine("" +
+                            "OutputSearchPrintingNo," +
+                            "OutputSearchPrintingCompanyCode," +
+                            "OutputSearchPrintingFileName," +
+                            "OutputSearchPrintingFullPath," +
+                            "OutputSearchPrintingDowloadStatus," +
+                            "OutputSearchPrintingDowloadCount," +
+                            "OutputSearchPrintingDowloadLastTime," +
+                            "OutputSearchPrintingDowloadLastBy," +
+                            "CreateBy," +
+                            "CreateDate," +
+                            "UpdateBy," +
+                            "UpdateDate," +
+                            "Isactive");
+
+
+
+                        foreach (var item in tran)
+                        {
+                            strBuilder.AppendLine($"" +
+                                $"{item.OutputSearchPrintingNo}," +
+                                $"{item.OutputSearchPrintingCompanyCode}," +
+                                $"{item.OutputSearchPrintingFileName}," +
+                                $"{item.OutputSearchPrintingFullPath}," +
+                                $"{item.OutputSearchPrintingDowloadStatus}," +
+                                $"{item.OutputSearchPrintingDowloadCount}," +
+                                $"{item.OutputSearchPrintingDowloadLastTime}," +
+                                $"{item.OutputSearchPrintingDowloadLastBy}," +
+                                $"{item.CreateBy}," +
+                                $"{item.CreateDate}," +
+                                $"{item.UpdateBy}," +
+                                $"{item.UpdateDate}," +
+                                $"{item.Isactive}");
+                        }
+
+                        resp.STATUS = true;
+                    }
+                    else
+                    {
+                        resp.STATUS = false;
+                    }
+                }
+                else
+                {
+                    ViewBag.Error = task.MESSAGE;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+            }
+
+            return File(Encoding.UTF8.GetBytes(strBuilder.ToString()), "text/csv", "scg-etax-OutputSearchPrinting.csv");
+
+        }
+
+
+    }
+}
