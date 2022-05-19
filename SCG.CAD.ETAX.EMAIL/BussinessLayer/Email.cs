9170b89d40c8ab142d93069c1164c8196131ec08
CADADT25 <CADADT25@scg.com>
2022-05-19 10:29:27 +0700

    commit

--- SCG.CAD.ETAX.EMAIL/BussinessLayer/Email.cs
+++ SCG.CAD.ETAX.EMAIL/BussinessLayer/Email.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SCG.CAD.ETAX.EMAIL.Controller
+namespace SCG.CAD.ETAX.EMAIL.BussinessLayer
 {
     public class Email
     {
---
+++ SCG.CAD.ETAX.EMAIL/Controller/OutputSearchXmlZipController.cs
@@ -0,0 +1,38 @@
+﻿using SCG.CAD.ETAX.MODEL.etaxModel;
+using SCG.CAD.ETAX.UTILITY;
+using Newtonsoft.Json;
+using SCG.CAD.ETAX.MODEL;
+
+namespace SCG.CAD.ETAX.EMAIL.Controller
+{
+    public class OutputSearchXmlZipController
+    {
+        public async Task<List<OutputSearchXmlZip>> List()
+        {
+            Response resp = new Response();
+
+            List<OutputSearchXmlZip> tran = new List<OutputSearchXmlZip>();
+
+            try
+            {
+                var task = await Task.Run(() => ApiHelper.GetURI("api/OutputSearchXmlZip/GetListAll"));
+
+                if (task.STATUS)
+                {
+                    tran = JsonConvert.DeserializeObject<List<OutputSearchXmlZip>>(task.OUTPUT_DATA.ToString());
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+
+
+            return tran;
+        }
+    }
+}
