be4800d9c597af5e8e5baff7b103f460ecd5db47
cadadt02 <cadadt02@scg.com>
2022-11-29 12:19:08 +0700

    Fixed issues select path of server

---
+++ SCG.CAD.ETAX.WEB/Controllers/Etax/DirectoryServerController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SCG.CAD.ETAX.WEB.Controllers.Etax
+{
+    public class DirectoryServerController : Controller
+    {
+        [SessionExpire]
+        public IActionResult GetPathServer_Modal()
+        {
+            return View();
+        }
+        public JsonResult GetPathServer_Forward(string currentPath, string name)
+        {
+            var data = new DirectoryServerDataModel();
+            var path = "";
+            var configTask = Task.Run(() => ApiHelper.GetURI("api/ConfigGlobal/GetDetailByName?cate=APPLICATION&name=ROOTPATHOFSERVER")).Result;
+            var config = new ConfigGlobal();
+            if (configTask.STATUS)
+            {
+                config = JsonConvert.DeserializeObject<ConfigGlobal>(configTask.OUTPUT_DATA.ToString());
+                path = config.ConfigGlobalValue;
+            }
+
+            if (!string.IsNullOrEmpty(currentPath))
+                path = currentPath;
+
+            if (!string.IsNullOrEmpty(name))
+                path = Path.Combine(path, name);
+
+            data.CurrentPath = path;
+            data.CurrentFolder = path.Split("\\").Last();
+
+            var filePaths = Directory.GetDirectories(path, "*").ToList();
+
+            if (filePaths != null)
+            {
+                filePaths.ForEach(t =>
+                {
+                    data.ChildFolderList.Add(t.Split("\\").Last());
+                });
+            }
+
+            return Json(data);
+        }
+        public JsonResult GetPathServer_Back(string path)
+        {
+            var data = new DirectoryServerDataModel();
+
+            data.CurrentPath = path;
+            data.CurrentFolder = path.Split("\\").Last();
+
+            var filePaths = Directory.GetDirectories(path, "*").ToList();
+
+            if (filePaths != null)
+            {
+                filePaths.ForEach(t =>
+                {
+                    data.ChildFolderList.Add(t.Split("\\").Last());
+                });
+            }
+
+            return Json(data);
+        }
+    }
+}
