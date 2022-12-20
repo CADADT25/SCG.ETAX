ad21395c65511e9cf3731a45d3ba19ac63ea0426
CADADT25 <CADADT25@scg.com>
2022-12-20 16:24:03 +0700

    Edit template and data process xmlgen

---
+++ SCG.CAD.ETAX.UTILITY/Controllers/UtilityProfileReasonIssueController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using SCG.CAD.ETAX.MODEL;
+using SCG.CAD.ETAX.MODEL.etaxModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCG.CAD.ETAX.UTILITY.Controllers
+{
+    public class UtilityProfileReasonIssueController
+    {
+        public async Task<List<ProfileReasonIssue>> List()
+        {
+            Response resp = new Response();
+
+            List<ProfileReasonIssue> tran = new List<ProfileReasonIssue>();
+
+            try
+            {
+                var task = await Task.Run(() => ApiHelper.GetURI("api/ProfileReasonIssue/GetListAll"));
+
+                if (task.STATUS)
+                {
+                    tran = JsonConvert.DeserializeObject<List<ProfileReasonIssue>>(task.OUTPUT_DATA.ToString());
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+
+
+            return tran;
+        }
+    }
+}
--- SCG.CAD.ETAX.UTILITY/Controllers/UtilityXMLGenerateController.cs
+++ SCG.CAD.ETAX.UTILITY/Controllers/UtilityXMLGenerateController.cs
@@ -17,6 +17,7 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
         UtilityProfileController profileController = new UtilityProfileController();
         UtilityTransactionDescriptionController transactionDescription = new UtilityTransactionDescriptionController();
         UtilityProfileBranchController profileBranchController = new UtilityProfileBranchController();
+        UtilityProfileReasonIssueController profileReasonIssueController = new UtilityProfileReasonIssueController();
         UtilityProductUnitController productUnitController = new UtilityProductUnitController();
         UtilityConfigGlobalController configGlobalController = new UtilityConfigGlobalController();
         LogicToolHelper logicToolHelper = new LogicToolHelper();
@@ -32,6 +33,7 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
             List<TransactionDescription> listdatatransactionDescription = new List<TransactionDescription>();
             List<TransactionDescription> datatransactionDescription = new List<TransactionDescription>();
             List<ProfileBranch> profileBranches = new List<ProfileBranch>();
+            List<ProfileReasonIssue> profileReasonIssue = new List<ProfileReasonIssue>();
             List<ProductUnit> productUnit = new List<ProductUnit>();
             List<ConfigGlobal> configGlobal = new List<ConfigGlobal>();
 
@@ -101,6 +103,13 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
                 }
                 profileBranches = (List<ProfileBranch>)res.OUTPUT_DATA;
 
+                res = GetProfileReasonIssue();
+                if (res.STATUS == false)
+                {
+                    return res;
+                }
+                profileReasonIssue = (List<ProfileReasonIssue>)res.OUTPUT_DATA;
+
                 res = GetConfigGlobal();
                 if (res.STATUS == false)
                 {
@@ -147,7 +156,7 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
                                         }
                                         else
                                         {
-                                            res = ConvertClasstoXMLFormat(classtextfile, documentCode, profileCompany, profileSeller, taxCode);
+                                            res = ConvertClasstoXMLFormat(classtextfile, documentCode, profileCompany, profileSeller, taxCode, profileReasonIssue);
                                             if (res.STATUS)
                                             {
                                                 var dataxml = (CrossIndustryInvoice)res.OUTPUT_DATA;
@@ -455,6 +464,21 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
             {
                 res.STATUS = true;
                 res.OUTPUT_DATA = profileBranchController.List().Result;
+            }
+            catch (Exception ex)
+            {
+                res.STATUS = false;
+                res.ERROR_MESSAGE = ex.ToString();
+            }
+            return res;
+        }
+        public Response GetProfileReasonIssue()
+        {
+            Response res = new Response();
+            try
+            {
+                res.STATUS = true;
+                res.OUTPUT_DATA = profileReasonIssueController.List().Result;
             }
             catch (Exception ex)
             {
@@ -841,7 +865,7 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
             return res;
         }
 
-        public Response ConvertClasstoXMLFormat(TextFileSchematic data, List<DocumentCode> documentCode, List<ProfileCompany> profileCompany, List<ProfileSeller> profileSeller, List<TaxCode> taxCode)
+        public Response ConvertClasstoXMLFormat(TextFileSchematic data, List<DocumentCode> documentCode, List<ProfileCompany> profileCompany, List<ProfileSeller> profileSeller, List<TaxCode> taxCode, List<ProfileReasonIssue> profileReasonIssue)
         {
             Response res = new Response();
             res.STATUS = false;
@@ -854,6 +878,7 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
                 ProfileSeller profile = new ProfileSeller();
                 ProfileCompany profiledetail = new ProfileCompany();
                 DocumentCode doccode = new DocumentCode();
+                DocumentCode refdoccode = new DocumentCode();
                 string schemeID = "OTHR";
                 xmldata = new CrossIndustryInvoice();
                 supplyChainTradeTransaction = new SupplyChainTradeTransaction();
@@ -878,6 +903,13 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
                 xmldata.exchangedDocument.typeCode = doccode.DocumentCodeRd ?? "";
                 xmldata.exchangedDocument.issueDateTime = data.BILLING_DATE ?? "";
                 xmldata.exchangedDocument.createionDateTime = data.CREATE_DATE_TIME ?? "";
+                xmldata.exchangedDocument.purpose = data.ORDER_REASON;
+                xmldata.exchangedDocument.purposeCode = "";
+                var reasoncode = profileReasonIssue.FirstOrDefault(x => x.ReasonIssueErpReasonCode == data.ORDER_REASON_CODE);
+                if(reasoncode != null)
+                {
+                    xmldata.exchangedDocument.purposeCode = reasoncode.ReasonIssueRdReasonCode ?? "";
+                }
 
                 profile = profileController.ProfileAddress(profileSeller, profiledetail.CompanyCode, data.SELLER_BRANCH);
                 supplyChainTradeTransaction.applicableHeaderTradeAgreement = new ApplicableHeaderTradeAgreement();
@@ -923,9 +955,15 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
                 supplyChainTradeTransaction.applicableHeaderTradeAgreement.buyerTradeParty.postalTradeAddress.countryID = data.BUYER_LAND1 ?? "";
 
                 supplyChainTradeTransaction.applicableHeaderTradeAgreement.additionalReferencedDocument = new AdditionalReferencedDocument();
-                supplyChainTradeTransaction.applicableHeaderTradeAgreement.additionalReferencedDocument.referenceTypeCode = "";
-                supplyChainTradeTransaction.applicableHeaderTradeAgreement.additionalReferencedDocument.issueDateTime = "";
-                supplyChainTradeTransaction.applicableHeaderTradeAgreement.additionalReferencedDocument.issuerAssignedID = "";
+
+                supplyChainTradeTransaction.applicableHeaderTradeAgreement.additionalReferencedDocument.referenceTypeCode = data.REF_FI_DOCTYPE ?? "";
+                supplyChainTradeTransaction.applicableHeaderTradeAgreement.additionalReferencedDocument.issueDateTime = data.REF_DOC_DATE ?? "";
+                supplyChainTradeTransaction.applicableHeaderTradeAgreement.additionalReferencedDocument.issuerAssignedID = data.REF_DOC ?? "";
+                refdoccode = documentCode.FirstOrDefault(x => x.DocumentCodeErp == (data.REF_FI_DOCTYPE ?? ""));
+                if(refdoccode != null)
+                {
+                    supplyChainTradeTransaction.applicableHeaderTradeAgreement.additionalReferencedDocument.referenceTypeCode = refdoccode.DocumentCodeRd ?? "";
+                }
 
                 supplyChainTradeTransaction.applicableHeaderTradeSettlement = new ApplicableHeaderTradeSettlement();
                 supplyChainTradeTransaction.applicableHeaderTradeSettlement.invoiceCurrencyCode = new InvoiceCurrencyCode();
@@ -943,6 +981,8 @@ namespace SCG.CAD.ETAX.UTILITY.Controllers
                 supplyChainTradeTransaction.applicableHeaderTradeSettlement.specifiedTradeSettlementHeaderMonetarySummation.taxTotalAmount = data.TAX_TOTAL_AMOUNT ?? "";
                 supplyChainTradeTransaction.applicableHeaderTradeSettlement.specifiedTradeSettlementHeaderMonetarySummation.grandTotalAmount = data.GRAND_TOTAL_AMOUNT ?? "";
                 supplyChainTradeTransaction.applicableHeaderTradeSettlement.specifiedTradeSettlementHeaderMonetarySummation.chargeTotalAmount = data.CHARGE_AMOUNT ?? "";
+                supplyChainTradeTransaction.applicableHeaderTradeSettlement.specifiedTradeSettlementHeaderMonetarySummation.originalInformationAmount = data.ORIGINAL_AMOUNT ?? "";
+                supplyChainTradeTransaction.applicableHeaderTradeSettlement.specifiedTradeSettlementHeaderMonetarySummation.differenceSalesInformationAmount = data.DIFFERENCE_AMOUNT ?? "";
 
                 supplyChainTradeTransaction.includedSupplyChainTradeLineItem = new List<IncludedSupplyChainTradeLineItem>();
                 for (int i = 0; i < data.Item.Count; i++)
